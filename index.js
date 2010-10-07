66be6ba9ac8539386eddee4d0d1f7300d6c2c3e1
delza <delza@livingcode.org>
2010-10-07 07:46:29 -0700

    still updating hello world

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ app.get('/', function(req, res){
     res.send('Hello World');
 });
 
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyDecoder());
+    app.use(app.router);
+    app.use(express.staticProvider(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
+
 // start listening for connections
 app.listen(5000);
 console.log('Express server started on port %s', app.address().port);
